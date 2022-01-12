66e32c9bcc2c395d23398f6162074d602f291be0
Christian Vila <christian.leandro.vila@gmail.com>
2022-01-12 00:31:15 -0300

    userOK

--- AppTorneo/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ApptorneoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'AppTorneo'
--- AppTorneo/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-class TorneoFormulario (forms.Form):
-    
-    nombre = forms.CharField(max_length=40)
-    tipo= forms.CharField(max_length=40)
-    categoria = forms.CharField(max_length=40)
-    sede = forms.CharField(max_length=40)
-    inicio = forms.DateField(input_formats= ["%d/%m/%Y"],help_text= "Ingrese dd/mm/aaaa")
